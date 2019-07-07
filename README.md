f58ed0f2a0e488ada3cd548548d0e35754827de1
maxdevbp <maxdevbp@gmail.com>
2019-07-07 11:49:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-max-credit-club
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-max-credit-club)
---
+++ src/app/credit-card/credit-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {CreditCardService} from './credit-card.service'
+
+@Component({
+  selector: 'app-credit-card',
+  templateUrl: './credit-card.component.html',
+  styleUrls: ['./credit-card.component.css'],
+  providers:[CreditCardService]
+})
+export class CreditCardComponent implements OnInit {
+
+  constructor(public service: CreditCardService) { }
+
+  ngOnInit() {
+  }
+
+}
