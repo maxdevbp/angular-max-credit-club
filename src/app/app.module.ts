a99418f3bc067357c2ba299d91114333810901a9
maxdevbp <maxdevbp@gmail.com>
2019-07-18 14:21:36 +0300

    2 files modified, 4 added, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { CreditCardComponent } from './credit-card/credit-card.component';
-import { CreditCardService } from './credit-card/credit-card.service';
+import { UsersComponent } from './users/users.component';
+import { UsersService } from './users/users.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CreditCardComponent ],
-  bootstrap:    [ AppComponent ],
-  providers: [CreditCardService]
+  imports: [BrowserModule, FormsModule , ReactiveFormsModule ,HttpClientModule],
+  declarations: [AppComponent, HelloComponent,  UsersComponent],
+  bootstrap: [AppComponent],
+  providers: [ UsersService]
 })
 export class AppModule { }
