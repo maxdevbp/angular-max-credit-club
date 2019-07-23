04fb33458036b435db29d73f9688fad9498fee25
maxdevbp <maxdevbp@gmail.com>
2019-07-23 14:54:29 +0300

    4 files modified, 2 added

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './users.service';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from "@angular/forms";
 
 @Component({
   selector: 'app-users',
@@ -7,11 +8,29 @@ import { UsersService } from './users.service';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: any[];
+
+  public userForm: FormGroup = new FormGroup({
+    id: new FormControl("", [Validators.required]),
+    first_name: new FormControl("", [Validators.required]),
+    last_name: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    avatar: new FormControl("", [Validators.required]),
+  })
 
   constructor(private service: UsersService) { }
 
   ngOnInit() {
-    
+     this.service.getUsers().subscribe(data => {
+      this.users = data.data;
+    })
   }
 
+  push(user: any) {
+    console.log('user: ', user);
+    if (!this.users) {
+      this.users = [];
+    }
+    this.users.push(user);
+  }
 }
