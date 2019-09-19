e5750c6e91731006f3d85c6fa1b931ab5334d680
maxdevbp <maxdevbp@gmail.com>
2019-09-19 14:39:51 +0300

    2 files modified

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './users.service';
+import { FormGroup, FormControl, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -8,12 +9,33 @@ import { UsersService } from './users.service';
 })
 export class UsersComponent implements OnInit {
   users = [];
+  userForm :FormGroup ;
   constructor(private service: UsersService) { }
 
   ngOnInit() {
-    this.service.getUsers().subscribe((data: any[]) => {
-      this.users = data.slice();
+    this.service.getUsers().subscribe((data) => {
+      var users = <any[]>(data['data']);
+      this.users = users.slice();
     })
+
+    this.userForm =new FormGroup({
+      'firstName' : new FormControl('', Validators.required),
+      'lastName' : new FormControl('', Validators.required),
+      'email' : new FormControl('', [Validators.required, 
+                                     Validators.email]),
+      'imagePath' : new FormControl('', Validators.required)
+    })
+  }
+
+  onSubmit(){
+    var user =  {email : this.userForm.value["email"], first_name: this.userForm.value["firstName"], last_name: this.userForm.value["firstName"], avatar: this.userForm.value["imagePath"]};
+    console.log(this.userForm)
+    // user.email = this.userForm.value["email"];
+    // user.first_name = this.userForm.value["firstName"];
+    // user.last_name = this.userForm.value["lastlastName_name"];
+    // user.avatar = this.userForm.value["imagePath"];
+    this.users.push(user);
+    
   }
 
 }
